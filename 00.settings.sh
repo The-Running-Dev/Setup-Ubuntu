53ab8e192f23003ad1c7352fc9dfde03459bc07c
The Running Dev <therunningdev@gmail.com>
2019-10-31 13:24:27 +0100

    - Installed GitHub ceritficate in remote install

--- 00.settings.sh
+++ 00.settings.sh
@@ -7,10 +7,8 @@ gitHubCertificate="$dir/github-com.pem"
 gitHubGistCertificate="$dir/gist.github-com.pem"
 gitHubUserContentCertificate="$dir/gist.githubusercontent-com.pem"
 
-#openssl s_client -showcerts -servername github.com -connect github.com:443 </dev/null 2>/dev/null | sed -n -e '/BEGIN\ CERTIFICATE/,/END\ CERTIFICATE/ p'  > github-com.pem
 cat $gitHubCertificate | sudo tee -a /etc/ssl/certs/ca-certificates.crt > /dev/null 2>&1
 
-#openssl s_client -showcerts -servername gist.githubusercontent.com -connect gist.githubusercontent.com:443 </dev/null 2>/dev/null | sed -n -e '/BEGIN\ CERTIFICATE/,/END\ CERTIFICATE/ p'  > gist.githubusercontent-com.pem
 cat $gitHubGistCertificate | sudo tee -a /etc/ssl/certs/ca-certificates.crt > /dev/null 2>&1
 
 cat $gitHubUserContentCertificate | sudo tee -a /etc/ssl/certs/ca-certificates.crt > /dev/null 2>&1
--- install.remote.sh
+++ install.remote.sh
@@ -1,5 +1,14 @@
 #!/bin/bash -e
 
+gitHubCertificate="/tmp/github-com.pem"
+
+# Download and install the GitHub certificate locally
+openssl s_client -showcerts -servername github.com -connect github.com:443 </dev/null 2>/dev/null | sed -n -e '/BEGIN\ CERTIFICATE/,/END\ CERTIFICATE/ p'  > $gitHubCertificate
+cat $gitHubCertificate | sudo tee -a /etc/ssl/certs/ca-certificates.crt > /dev/null 2>&1
+
 git clone https://github.com/The-Running-Dev/Setup-Ubuntu.git /tmp/Setup-Ubuntu/
-sudo chmod +x /tmp/Setup-Ubuntu/*.sh
-/tmp/Setup-Ubuntu/install.sh
+
+if [ -d /tmp/Setup-Ubuntu/ ]; then
+    sudo chmod +x /tmp/Setup-Ubuntu/*.sh
+    /tmp/Setup-Ubuntu/install.sh
+fi
