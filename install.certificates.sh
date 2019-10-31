b2d0a93658a42c7095a14af3aa6e3d77688260e3
The Running Dev <therunningdev@gmail.com>
2019-10-31 21:50:51 +0100

    - Forgot to add the new files

---
+++ install.certificates.sh
@@ -0,0 +1,20 @@
+#!/bin/bash -e
+
+# Download and install the GitHub certificates
+openssl s_client -showcerts -servername github.com \
+    -connect github.com:443 </dev/null 2>/dev/null | \
+    sed -n -e '/BEGIN\ CERTIFICATE/,/END\ CERTIFICATE/ p' \
+    | \
+    sudo tee -a /etc/ssl/certs/ca-certificates.crt > /dev/null 2>&1
+
+openssl s_client -showcerts -servername gist.github.com \
+    -connect gist.github.com:443 </dev/null 2>/dev/null | \
+    sed -n -e '/BEGIN\ CERTIFICATE/,/END\ CERTIFICATE/ p' \
+    | \
+    sudo tee -a /etc/ssl/certs/ca-certificates.crt > /dev/null 2>&1
+
+openssl s_client -showcerts -servername gist.githubusercontent.com \
+    -connect gist.githubusercontent.com:443 </dev/null 2>/dev/null | \
+    sed -n -e '/BEGIN\ CERTIFICATE/,/END\ CERTIFICATE/ p' \
+    | \
+    sudo tee -a /etc/ssl/certs/ca-certificates.crt > /dev/null 2>&1
--- install.remote.sh
+++ install.remote.sh
@@ -1,15 +1,7 @@
 #!/bin/bash -e
 
-# Ask for the user password & cache it
-sudo true
-
 repositoryUrl='https://github.com/The-Running-Dev/Setup-Ubuntu.git'
-gitHubCertificate='/tmp/github-com.pem'
 cloneDir='/tmp/Setup-Ubuntu/'
-
-# Download and install the GitHub certificate locally
-openssl s_client -showcerts -servername github.com -connect github.com:443 </dev/null 2>/dev/null | sed -n -e '/BEGIN\ CERTIFICATE/,/END\ CERTIFICATE/ p' > $gitHubCertificate
-cat $gitHubCertificate | sudo tee -a /etc/ssl/certs/ca-certificates.crt > /dev/null 2>&1
 
 git clone $repositoryUrl $cloneDir
 
