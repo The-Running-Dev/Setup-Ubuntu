4d78e614acffdd8a72e5ac7a232dfc2078579fe1
The Running Dev <therunningdev@gmail.com>
2019-10-31 21:49:46 +0100

    - Created separate install script for certs - Removed certs install from remote script

--- 00.settings.sh
+++ 00.settings.sh
@@ -1,14 +1,1 @@
 #!/bin/bash -e
-
-# Get the script directory
-dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-
-gitHubCertificate="$dir/github-com.pem"
-gitHubGistCertificate="$dir/gist.github-com.pem"
-gitHubUserContentCertificate="$dir/gist.githubusercontent-com.pem"
-
-cat $gitHubCertificate | sudo tee -a /etc/ssl/certs/ca-certificates.crt > /dev/null 2>&1
-
-cat $gitHubGistCertificate | sudo tee -a /etc/ssl/certs/ca-certificates.crt > /dev/null 2>&1
-
-cat $gitHubUserContentCertificate | sudo tee -a /etc/ssl/certs/ca-certificates.crt > /dev/null 2>&1
