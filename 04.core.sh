a7e2691651c931ce86a397dcd2645aa27fa4512e
Ben Richards <therunningdev@gmail.com>
2020-11-23 19:41:59 +0100

    Updating >NET Repository and Packages

--- 04.core.sh
+++ 04.core.sh
@@ -2,12 +2,16 @@
 
 # Source: https://dotnet.microsoft.com/learn/dotnet/hello-world-tutorial/install
 echo "Downloading the Microsoft Repository GPG Keys..."
-wget -q https://packages.microsoft.com/config/ubuntu/18.04/packages-microsoft-prod.deb
+wget -q https://packages.microsoft.com/config/ubuntu/20.04/packages-microsoft-prod.deb
+
+# Register the Microsoft repository GPG keys
 sudo dpkg -i packages-microsoft-prod.deb
 
+# Update the list of products
 echo "Updating Repositories..."
 sudo apt-get update
 
+# Enable the "universe" repositories
 echo "Adding the Universe Repository..."
 sudo add-apt-repository universe
 
@@ -15,6 +19,7 @@ echo "Installing PowerShell and .NET Core..."
 sudo apt-get install \
     powershell \
     dotnet-sdk-3.1 \
+    dotnet-sdk-5.0 \
     -y
 
 echo "Installing Azure CLI..."
